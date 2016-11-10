6a0511b43a599ca529b16f33e991f7d9d34a6167
Aleksandr Bogomolov <ksandr.ua@gmail.com>
2016-11-10 19:55:15 +0200

    Add get users to angular users controller

--- src/main/resources/static/js/controllers/users.js
+++ src/main/resources/static/js/controllers/users.js
@@ -1,5 +1,13 @@
 'use strict';
 
-app.controller('users', ['$scope', function ($scope) {
-    $scope.users = 'Куку епта'
-}]);
+app.controller('users', function ($scope, $http) {
+    $scope.users = [];
+    var users = $http.get('/user');
+    users.then(function (users) {
+        $scope.users = users.data;
+        var homeLi = $.getElementById('#home');
+        homeLi.$removeClass('active');
+        var userLi = $.getElementById('#users');
+        userLi.addClass('active');
+    })
+});
