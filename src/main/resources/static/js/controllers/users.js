773a9b370da0fad9c37d36b9c5eabce84e148ba1
Aleksandr Bogomolov <ksandr.ua@gmail.com>
2016-11-11 16:17:08 +0200

    Add table, modal form to user

--- src/main/resources/static/js/controllers/users.js
+++ src/main/resources/static/js/controllers/users.js
@@ -2,12 +2,18 @@
 
 app.controller('users', function ($scope, $http) {
     $scope.users = [];
-    var users = $http.get('/user');
-    users.then(function (users) {
+
+    $http.get('/user').then(function (users) {
         $scope.users = users.data;
-        var homeLi = $.getElementById('#home');
-        homeLi.$removeClass('active');
-        var userLi = $.getElementById('#users');
-        userLi.addClass('active');
-    })
+    });
+
+    $scope.editUser = function (id) {
+        var form = $('#edit-user');
+        $http.get('/user/' + id).then(function (user) {
+            angular.forEach(user.data, function (value, key) {
+                angular.element(form).find("input[name='" + key + "']").val(value);
+            })
+        });
+        form.modal();
+    }
 });
