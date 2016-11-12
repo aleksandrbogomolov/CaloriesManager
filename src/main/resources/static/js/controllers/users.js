9a52f285a5b2ae4ab33c8f049dec53a0109e9cc2
Aleksandr Bogomolov <ksandr.ua@gmail.com>
2016-11-12 15:37:03 +0200

    Some refactoring in angular submit User

--- src/main/resources/static/js/controllers/users.js
+++ src/main/resources/static/js/controllers/users.js
@@ -20,19 +20,12 @@ app.controller('users', function ($scope, $http) {
 
     $scope.submitUser = function () {
         var user = {};
-        var data = angular.element('#details-form').serialize().split('&');
-        angular.forEach(data, function (value) {
+        var form = angular.element('#details-form').serialize().split('&');
+        angular.forEach(form, function (value) {
             var tmp = value.split('=');
             user[tmp[0]] = tmp[1];
-            if (tmp[0] == 'email') user[tmp[0]] = tmp[1].replace(/%40/g, '@');
-            if (tmp[0] == 'roles') {
-                var raw = tmp[1].split('%2C');
-                var roles = [];
-                angular.forEach(raw, function (value) {
-                    roles.push(value.trim());
-                });
-                user[tmp[0]] = roles;
-            }
+            if (tmp[0] == 'email') user[tmp[0]] = tmp[1].replace('%40', '@');
+            if (tmp[0] == 'roles') user[tmp[0]] = tmp[1].split('%2C');
             if (tmp[0] == 'createdDate') user[tmp[0]] = tmp[1].replace(/%2C/g, '-');
         });
         $http.post('/user', user).then(function () {
