9b71a7d9d64a150d6f62330c0bc37d69a8a3f333
Aleksandr Bogomolov <ksandr.ua@gmail.com>
2016-11-23 11:03:04 +0200

    Add smart table to users.html, refactoring users.js

--- src/main/resources/static/js/controllers/users.js
+++ src/main/resources/static/js/controllers/users.js
@@ -6,7 +6,7 @@ app.controller('users', function ($scope, $http) {
 
     $scope.getAll = function () {
         $http.get(usersUrl).then(function (users) {
-            $scope.users = users.data;
+            updateTable(users);
         });
     };
 
@@ -14,7 +14,7 @@ app.controller('users', function ($scope, $http) {
         var form = angular.element('#edit-user');
         $http.get(usersUrl + '/' + id).then(function (user) {
             angular.forEach(user.data, function (value, key) {
-                form.find("input[name='" + key + "']").val(value);
+                form.find("input[name='" + key + "']").val(key != 'createdDate' ? value : parseDate(value));
             })
         });
         form.modal();
@@ -25,14 +25,7 @@ app.controller('users', function ($scope, $http) {
         var form = angular.element('#details-form').serialize().split('&');
         angular.forEach(form, function (value) {
             var tmp = value.split('=');
-            user[tmp[0]] = tmp[1];
-            if (tmp[0] == 'email') user[tmp[0]] = tmp[1].replace('%40', '@');
-            if (tmp[0] == 'roles') user[tmp[0]] = tmp[1].split('%2C');
-            if (tmp[0] == 'createdDate') {
-                var str = tmp[1].replace(/%2C/g, '-');
-                if (str.length < 10) str = [str.slice(0, 8), '0', str.slice(8)].join('');
-                user[tmp[0]] = str;
-            }
+            user[tmp[0]] = tmp[0] != 'roles' ? decodeURIComponent(tmp[1]) : decodeURIComponent(tmp[1]).split(',');
         });
         $http.post(usersUrl, user).then(function () {
             angular.element('#edit-user').modal('hide');
@@ -46,6 +39,24 @@ app.controller('users', function ($scope, $http) {
             angular.element('.table').val('');
             $scope.getAll();
         })
+    };
+
+    var parseDate = function (data) {
+        var month = data[1].toString().length == 1 ? '0' + data[1] : data[1];
+        var day = data[2].toString().length == 1 ? '0' + data[2] : data[2];
+        return data[0] + '-' + month + '-' + day;
+    };
+
+    var updateTable = function (users) {
+        var usersCollection = [];
+        angular.forEach(users.data, function (value) {
+            var user = {};
+            angular.forEach(value, function (data, key) {
+                user[key] = key != 'createdDate' ? data : parseDate(data);
+            });
+            usersCollection.push(user);
+        });
+        $scope.rowCollection = usersCollection;
     };
 
     $scope.getAll();
