2e8cd8cc16da334d1430b3460cfb85c4fd87465b
Aleksandr Bogomolov <ksandr.ua@gmail.com>
2016-11-24 16:55:51 +0200

    Add more data to populate_mongo.js

--- populate_mongo.js
+++ populate_mongo.js
@@ -45,23 +45,97 @@ mongoDB.meals.update(
     {
         "_id": "3",
         "_class": "com.aleksandrbogomolov.domain.Meal",
-        "dateTime": new Date("2016-01-16T07:25:50"),
+        "dateTime": new Date("2016-01-16T05:25:50.000Z"),
         "description": "Завтрак",
         "calories": 500,
-        "user": {"$ref": "users", "$id": "1"}
-    }, {"upsert": true}
-);
+        "user": {
+            "$ref": "users",
+            "$id": "1"
+        }
+    }, {"upsert": true});
 
 mongoDB.meals.update(
     {"_id": "4"},
     {
         "_id": "4",
         "_class": "com.aleksandrbogomolov.domain.Meal",
-        "dateTime": new Date("2016-11-16T12:25:50"),
+        "dateTime": new Date("2016-11-16T10:25:50.000Z"),
         "description": "Обед",
         "calories": 1000,
-        "user": {"$ref": "users", "$id": "1"}
-    }, {"upsert": true}
-);
+        "user": {
+            "$ref": "users",
+            "$id": "1"
+        }
+    }, {"upsert": true});
+
+mongoDB.meals.update(
+    {"_id": "5"},
+    {
+        "_id": "5",
+        "_class": "com.aleksandrbogomolov.domain.Meal",
+        "dateTime": new Date("2016-11-20T11:00:00.000Z"),
+        "description": "Полдник",
+        "calories": 995,
+        "user": {
+            "$ref": "users",
+            "$id": "1"
+        }
+    }, {"upsert": true});
+
+mongoDB.meals.update(
+    {"_id": "6"},
+    {
+        "_id": "6",
+        "_class": "com.aleksandrbogomolov.domain.Meal",
+        "dateTime": new Date("2016-11-20T14:00:00.000Z"),
+        "description": "Ужин",
+        "calories": 700,
+        "user": {
+            "$ref": "users",
+            "$id": "1"
+        }
+    }, {"upsert": true});
+
+mongoDB.meals.update(
+    {"_id": "7"},
+    {
+        "_id": "7",
+        "_class": "com.aleksandrbogomolov.domain.Meal",
+        "dateTime": new Date("2016-11-21T10:20:00.000Z"),
+        "description": "Обед",
+        "calories": 1000,
+        "user": {
+            "$ref": "users",
+            "$id": "1"
+        }
+    }, {"upsert": true});
+
+mongoDB.meals.update(
+    {"_id": "8"},
+    {
+        "_id": "8",
+        "_class": "com.aleksandrbogomolov.domain.Meal",
+        "dateTime": new Date("2016-11-21T06:05:00.000Z"),
+        "description": "Завтрак",
+        "calories": 400,
+        "user": {
+            "$ref": "users",
+            "$id": "1"
+        }
+    }, {"upsert": true});
+
+mongoDB.meals.update(
+    {"_id": "9"},
+    {
+        "_id": "9",
+        "_class": "com.aleksandrbogomolov.domain.Meal",
+        "dateTime": new Date("2016-11-21T13:00:00.000Z"),
+        "description": "Полдник",
+        "calories": 1000,
+        "user": {
+            "$ref": "users",
+            "$id": "1"
+        }
+    }, {"upsert": true});
 
 print('populate complete');
