9dcb5365b7ee4ec845acb2ce2ab47fd303fa73b6
Aleksandr Bogomolov <ksandr.ua@gmail.com>
2016-11-25 13:02:29 +0200

    Refactoring AbstractTest

--- src/test/java/com/aleksandrbogomolov/AbstractTest.java
+++ src/test/java/com/aleksandrbogomolov/AbstractTest.java
@@ -24,15 +24,15 @@ public abstract class AbstractTest {
     @Autowired
     protected MongoTemplate template;
 
-    protected int startId = 1;
+    private int startId = 1;
 
-    protected final User loggedUser = new User(String.valueOf(startId), "User", "user@mail.ru", "password", 2000, true, LocalDate.of(2016, 11, 7), new HashSet<>(Collections.singletonList(Role.ROLE_USER)));
+    protected final User loggedUser = new User("" + startId, "User", "user@mail.ru", "password", 2000, true, LocalDate.of(2016, 11, 7), new HashSet<>(Collections.singletonList(Role.ROLE_USER)));
 
-    protected final User testUser1 = new User(String.valueOf(startId + 1), "test", "test1@mail.ru", "test", 2000, true, LocalDate.of(2016, 11, 7), new HashSet<>(Collections.singletonList(Role.ROLE_USER)));
+    protected final User testUser1 = new User("" + (startId + 1), "test", "test1@mail.ru", "test", 2000, true, LocalDate.of(2016, 11, 7), new HashSet<>(Collections.singletonList(Role.ROLE_USER)));
 
-    protected final User testUser2 = new User(String.valueOf(startId + 2), "test2", "test2@mail.ru", "pass2", 2000, true, LocalDate.of(2016, 11, 8), new HashSet<>(Collections.singletonList(Role.ROLE_USER)));
+    protected final User testUser2 = new User("" + (startId + 2), "test2", "test2@mail.ru", "pass2", 2000, true, LocalDate.of(2016, 11, 8), new HashSet<>(Collections.singletonList(Role.ROLE_USER)));
 
-    protected final Meal testMeal1 = new Meal(String.valueOf(startId + 3), LocalDateTime.of(2016, 11, 10, 10, 8), "Завтрак", 500, false, loggedUser);
+    protected final Meal testMeal1 = new Meal("" + (startId + 3), LocalDateTime.of(2016, 11, 10, 10, 8), "Завтрак", 500, false, loggedUser);
 
-    protected final Meal testMeal2 = new Meal(String.valueOf(startId + 4), LocalDateTime.of(2016, 11, 12, 13, 0), "Обед", 1000, false, loggedUser);
+    protected final Meal testMeal2 = new Meal("" + (startId + 4), LocalDateTime.of(2016, 11, 12, 13, 0), "Обед", 1000, false, loggedUser);
 }
