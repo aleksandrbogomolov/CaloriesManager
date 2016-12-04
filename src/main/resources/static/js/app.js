1fc399a582675772bcdbad2fd4622383c9bbf0c1
Aleksandr Bogomolov <ksandr.ua@gmail.com>
2016-12-04 12:41:39 +0200

    Move route.js to app.js

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -2,3 +2,20 @@
 
 var app = angular.module('calories-manager', ['ngRoute', 'pascalprecht.translate', 'smart-table', 'ui.bootstrap.datetimepicker', 'ui.dateTimeInput']);
 
+app.config(function ($routeProvider, $httpProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'views/home.html',
+            controller: 'home'
+        })
+        .when('/users', {
+            templateUrl: 'views/users.html',
+            controller: 'users'
+        })
+        .when('/meals', {
+            templateUrl: 'views/meals.html',
+            controller: 'meals'
+        })
+        .otherwise('/');
+    $httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
+});
